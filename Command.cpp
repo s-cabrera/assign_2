7f354d42e88c140242a1d2b4b9e50a20c0a61fd9
Arturo Perez <apere129@hammer.cs.ucr.edu>
2018-02-20 08:51:43 -0800

    Edited class: Removal of temporary test code. vouid print()

--- Command.cpp
+++ Command.cpp
@@ -1,8 +1,7 @@
-//#include <vector>
-//#include <iostream>
+
+/* Comand Source File*/
 
 #include "Command.hpp"
-
 
 using namespace std;
 
@@ -10,51 +9,23 @@ void Command::parse(std::string a){
     string::iterator it;
     int i = 0;
     string tmp;
-    for ( it = a.begin() ; it < a.end(); it++,i++)
+    for ( it = a.begin() ; it < a.end(); it++,i++)   /* loop iteration on input*/
     {tmp=tmp+a[i];
         
            if (a[i]==38||a[i]==59||a[i]==124)
                {
-                
-                   this->connector.push_back(a[i]);
+                   this->connector.push_back(a[i]);  /* Store conector in data member*/
                  tmp.erase((tmp.size()-1),1);
           
-               
-               for (int j=0; j<tmp.size();j++)  //in separete func coud also erase spaces infront
-                {
+               for (int j=0; j<tmp.size();j++){      /* Parse trough array and separates comanss*/
                     if (tmp[0]==32)
-                    {
-                        tmp.erase(0,1);}}
-              
-               
+                    {tmp.erase(0,1);}}
                 this->commads.push_back(tmp);
-               
-               
                 tmp.clear();
-               
             if (a[i]!=59){ i++;}
-               
            }
-       else if (it==(a.end()-1))    /// gets lats comanf no separstor
-         //  else if (a[i]==NULL)
-        { this->commads.push_back(tmp);
-            
-        }
-
-       }
-    }
-void Command::p(){        //remove right
-    cout<<"contents of vector 1 then 2"<<endl;
-    for (int i=0;i<this->commads.size();i++ )
-    {
-        cout<<this->commads[i]<<endl;
-        
-        cout<<"conector "<<this->connector[i]<<endl;
+       else if (it==(a.end()-1))                     /* gets last command */
+        { this->commads.push_back(tmp);}
     }}
-
-
-
-
-
 
 
--- Command.hpp
+++ Command.hpp
@@ -1,21 +1,18 @@
 
+/* Comand Header File*/
 
 #ifndef COMMAND_H
 #define COMMAND_H
 #include <iostream>
 #include <vector>
 #include "CommandExecute.hpp"
-//using namespace std;
 
 class Command: public CommandExecute{
 public:
     void parse(std::string p);
-    void p();// remove
-    
-    
-private:
 
 };
 
 
 #endif /* Command_hpp */
+

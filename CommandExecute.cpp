578d53ccee0bb71b071f2aff96522eca9ddeebcc
Arturo Perez <apere129@hammer.cs.ucr.edu>
2018-02-20 08:57:51 -0800

    Edited source file: splited Execute function into two independet processes

--- CommandExecute.cpp
+++ CommandExecute.cpp
@@ -1,13 +1,12 @@
 
+
+/* ComandExecute Source File*/
 #include <iostream>
 #include <sys/wait.h>
 #include  <sys/types.h>
 #include <unistd.h>
 #include  <stdio.h>
 #include <string>
-
-//#include <string.h>
-//#include <sys/wait.h>
 #include "CommandExecute.hpp"
 using namespace std;
 
@@ -15,17 +14,13 @@ CommandExecute::CommandExecute(){}
 
 
 void CommandExecute::execute(){
-
-    
+    /* Setting up variables for execution*/
     string str;
     char *tmp[50];
-    
     tmp[0]="sh";
     tmp[1]="-c";
-    
-    for (int i=0;i<this->commads.size();i++)
-    {
-        
+    /* Executes comands in conatiner*/
+    for (int i=0;i<this->commads.size();i++){
     char buffer[99];
     str=this->commads[i];
     size_t length=str.copy(buffer, str.size(),0);
@@ -33,55 +28,32 @@ void CommandExecute::execute(){
     tmp[2]=buffer;
     tmp[3]=NULL;
     
-    cout<<"Execution: "<<endl;
-        
-    
-        
         bool sta=true;
         if (status==true)
-        {  sta=this->run(tmp);
-            //if (sta==false){}}
-        }
+        {  sta=this->run(tmp);}
        if (this->connector.empty()==false)
-       {
+       {/*Odered Execution based on Conectors */
             if (this->connector[i]==';'){
-                this->status=true;
-               // cout <<"hi ;"<<endl;
-            }
+                this->status=true;}
             else if (this->connector[i]=='&'){
                 if (sta==true){this->status=true;}
                 else{
                     this->status=false;
-                }
-            
-                //cout <<"hi &"<<endl;
-            }
+                }}
             else if (this->connector[i]=='|'){
                 if (sta==false){this->status=true;}
                 else{
-                    this->status=false;
-                }
-                
-               // cout <<"hi |"<<endl;
-            }
-           // else {
-             //   this->status=false;
-           // }
-       
-    }}
-
-}
-
-
-
+                    this->status=false;}}
+        }}}
 
 void CommandExecute::parse(std::string p)
 {}
 bool CommandExecute::run(char **a){
+    /* Forks parent and child processes for execution */
     bool tmp=true;
     pid_t  pid;
     int    status;
-    if ((pid = fork()) < 0) {     /* fork a child process           */
+    if ((pid = fork()) < 0) {     /* Start fork and child  */
         cout<<"* Unable to create fork child process"<<endl;
     }
     
@@ -89,19 +61,9 @@ bool CommandExecute::run(char **a){
         if (execvp("/bin/sh", a) < 0) {     /* execute the command  */
             cout<<"* unable to execute commnad"<<endl;
             tmp=false;
-        }
-    }
-    else {                                  /* for the parent:      */
-        while (wait(&status) != pid);
-    }
+        }}
+    else {
+        while (wait(&status) != pid);}
     return tmp;
 }
 
-void CommandExecute::p(){
-    std::cout<<"contents of vector 1 then 2"<<std::endl;
-for (int i=0;i<this->commads.size();i++ )
-{
-    std::cout<<this->commads[i]<<std::endl;
-    
-    std::cout<<"conector "<<this->connector[i]<<std::endl;
-}}
